12b785d6f78daff239ba31f8f75afbdedfa9ff6f
Akif Manzoor <akifmanzoor124@gmail.com>
2020-06-26 17:51:57 -0400

    Created Java classes for Java portion

---
+++ Comprehensive Assignment/Part 1 & 2/FaceTracker/OptimalAssignment.java
@@ -0,0 +1,15 @@
+
+public class OptimalAssignment{
+
+    /**
+     * Function: main
+     * Parameters: String[] args
+     * Return: None
+     * 
+     * Input: Two csv files which contain the detections of faces within the frames
+     */
+    public static void main(String[] args){
+        //frame1
+        //frame2
+    }
+}
